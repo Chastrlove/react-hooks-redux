024575cbbc85653ebad52343ce4c2cf6b454e39f
马晨杰 <309272985@qq.com>
2019-11-06 16:15:10 +0800

    chore:update components structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 import { FormDemo } from "components/FormDemo";
+import { Provider } from "react-redux";
+import { history, store } from "./redux/store";
+import { ConnectedRouter } from "connected-react-router";
+import { Route, Switch } from "react-router";
 
 const App = () => {
   return (
-    <>
-      <FormDemo />
-    </>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <>
+          <Switch>
+            <Route exact path="/" render={() => <FormDemo />} />
+            <Route render={() => <div>Miss</div>} />
+          </Switch>
+        </>
+      </ConnectedRouter>
+    </Provider>
   );
 };
 
